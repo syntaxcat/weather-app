cc86a821cf4e6275e4454d111cfb66f258d92761
Michaela Bereza <michaelabereza@icloud.com>
2023-09-23 19:39:44 +0300

    search locaion input api get locations

--- src/components/Locations/SearchLocation.tsx
+++ src/components/Locations/SearchLocation.tsx
@@ -1,23 +1,41 @@
-import React, {useState, useEffect} from "react"
+import React, {useState, useEffect, useCallback} from "react"
 import TextField from "@mui/material/TextField"
 import Autocomplete from "@mui/material/Autocomplete"
+import LocationOnIcon from "@mui/icons-material/LocationOn"
+import Grid from "@mui/material/Grid"
+import Typography from "@mui/material/Typography"
+
+interface City {
+  key: string
+  name: string
+  country: string
+}
+
+const apiKey = "Rstic0VBtv4jrY9KbUe2o4TzgybGMMlG"
+const END_POINT =
+  "https://dataservice.accuweather.com/locations/v1/cities/autocomplete"
 
 const SearchLocation = () => {
-  const [locations, setLocations] = useState([])
+  const [value, setValue] = useState<City | null>(null)
+  const [inputValue, setInputValue] = useState("")
+  const [options, setOptions] = useState<readonly City[]>([])
 
-  //   useEffect(() => {
-  //     fetchLoctionsHandler()
-  //   }, [])
+  //   const getData = () => {
+  //     setOptions(
+  //       CITIES.map((city) => {
+  //         return {
+  //           key: city.Key,
+  //           name: city.LocalizedName,
+  //           country: city.Country.LocalizedName
+  //         }
+  //       })
+  //     )
+  //   }
 
-  async function fetchLoctionsHandler() {
-    const apiKey = "KEY"
-    const query = "Tel"
-
-    await fetch(
-      `http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${apiKey}&q=${query}`
-    )
+  const sendRequest = useCallback(function fetchLoctionsHandler(userInput) {
+    fetch(`${END_POINT}?apikey=${apiKey}&q=${userInput}`)
       .then((response) => {
-        response.json()
+        return response.json()
       })
       .then((data) => {
         const foundLocations = data.map((locationData) => {
@@ -25,21 +43,68 @@ const SearchLocation = () => {
             name: locationData.LocalizedName
           }
         })
-        setLocations(foundLocations)
+        setOptions(foundLocations)
+        console.log(userInput)
       })
       .catch((error) => console.error("Error fetching locations:", error))
-  }
+    console.log("FETCHING")
+  }, [])
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      //   getData()
+      if (inputValue === "") {
+        setOptions(value ? [value] : [])
+        return
+      }
+      //   fetchLoctionsHandler()
+      sendRequest(inputValue)
+    }, 500)
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [inputValue])
+
   return (
-    <div>
-      <Autocomplete
-        disablePortal
-        id="combo-box-demo"
-        options={locations}
-        sx={{width: 300}}
-        // getOptionLabel={(option) => option.name}
-        renderInput={(params) => <TextField {...params} label="" />}
-      />
-    </div>
+    <Autocomplete
+      id="google-map-demo"
+      sx={{width: 300}}
+      getOptionLabel={(option) => option.name}
+      filterOptions={(x) => x}
+      options={options}
+      autoComplete
+      includeInputInList
+      filterSelectedOptions
+      value={value}
+      noOptionsText="No locations"
+      isOptionEqualToValue={(option, value) => {
+        return option.key === value.key
+      }}
+      onChange={(event: any, newValue: City | null) => {
+        setValue(newValue)
+      }}
+      onInputChange={(event, newInputValue) => {
+        setInputValue(newInputValue)
+      }}
+      renderInput={(params) => (
+        <TextField {...params} label="Add a location" fullWidth />
+      )}
+      renderOption={(props, option) => {
+        return (
+          <li {...props} key={props.key}>
+            <Grid container alignItems="center">
+              <Grid item sx={{display: "flex", width: 44}}>
+                <LocationOnIcon sx={{color: "text.secondary"}} />
+              </Grid>
+              {option.name}
+              <Typography variant="body2" color="text.secondary">
+                {option.country}
+              </Typography>
+            </Grid>
+          </li>
+        )
+      }}
+    />
   )
 }
 
