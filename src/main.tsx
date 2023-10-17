3acef7c668a8871353b072181c6578dc037f7d40
Michaela Bereza <michaelabereza@icloud.com>
2023-10-17 12:19:20 +0300

    return react strictMode for production

--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,19 @@ import {BrowserRouter} from "react-router-dom"
 import {StyledEngineProvider} from "@mui/material/styles"
 import {SnackbarProvider} from "notistack"
 
-// import React from "react"
+import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
 import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  // <React.StrictMode>
-  <StyledEngineProvider injectFirst>
-    <BrowserRouter>
-      <SnackbarProvider maxSnack={4}>
-        <App />
-      </SnackbarProvider>
-    </BrowserRouter>
-  </StyledEngineProvider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <StyledEngineProvider injectFirst>
+      <BrowserRouter>
+        <SnackbarProvider maxSnack={4}>
+          <App />
+        </SnackbarProvider>
+      </BrowserRouter>
+    </StyledEngineProvider>
+  </React.StrictMode>
 )
